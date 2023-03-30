8067096dac8f91ee3cb0c3058692a54c669a02ee
MohHussainIshraq <mhussain_ishraq@yahoo.com>
2023-03-30 20:47:26 +0430

    create about model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class About(models.Model):
+    title = models.CharField(max_length=48, verbose_name = 'عنوان')
+    description = models.TextField(verbose_name = 'توضیحات')
+    image = models.ImageField(upload_to="about_pictures", verbose_name = 'تصویر')
+
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = "در مورد توانا"
+        verbose_name_plural = "در موارد"
--- home/views.py
+++ home/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
-
+from home.models import About
 def home(request):
-    return render(request, "home/index.html", context={})
+    about = About.objects.all().last()
+
+    return render(request, "home/index.html", context={'about': about})
