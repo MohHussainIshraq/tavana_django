12aafa813ada8042b52c3a6955873d62f37db6ef
MohHussainIshraq <mhussain_ishraq@yahoo.com>
2023-03-30 18:17:58 +0430

    media_url and media_root added

--- tavana/settings.py
+++ tavana/settings.py
@@ -129,8 +129,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, "statics"),
 MEDIA_ROOT = os.path.join(BASE_DIR, "media",)
+STATICFILES_DIRS = os.path.join(BASE_DIR, "statics"),
 STATIC_ROOT = os.path.join(BASE_DIR, "static_files",)
 
 # Default primary key field type
