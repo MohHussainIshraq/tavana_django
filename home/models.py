02ca0d3e4ce3a1c1a4adca8bb3dc3d10b28acbf3
MohHussainIshraq <mhussain_ishraq@yahoo.com>
2023-04-01 08:56:45 +0430

    Details the budget, contact part, footer part implemented

--- home/models.py
+++ home/models.py
@@ -10,8 +10,8 @@ class About(models.Model):
 
 
     class Meta:
-        verbose_name = "در مورد توانا"
-        verbose_name_plural = "در موارد"
+        verbose_name = "در باره ما"
+        verbose_name_plural = "در باره ما"
 
 class Donation_Title(models.Model):
     title = models.CharField(max_length=50, verbose_name='عنوان')
@@ -61,3 +61,62 @@ class Pictures(models.Model):
         verbose_name = 'تصویر'
         verbose_name_plural = 'تصاویر'
 
+
+class Title_Expenses(models.Model):
+    title = models.CharField(max_length=40, verbose_name='عنوان')
+
+    def __str__(self):
+        return self.title
+    
+
+    class Meta:
+        verbose_name = 'عنوان بخش بودجه'
+        verbose_name_plural = 'عناوین بودجه'
+
+
+class Expenses(models.Model):
+    title = models.CharField(max_length=50, verbose_name='عنوان')
+    count = models.CharField(max_length=100, verbose_name='مقدار')
+
+    def __str__(self):
+        return self.title
+    
+
+    class Meta:
+        verbose_name = 'بودجه'
+        verbose_name_plural = 'بودجه'
+
+
+class Contact(models.Model):
+    title = models.CharField(max_length=100, verbose_name='عنوان')
+    description = models.TextField(verbose_name='توضیحات')
+    email = models.EmailField(verbose_name='ایمیل')
+    phone = models.CharField(max_length=100, verbose_name='شماره های تماس')
+    address = models.CharField(max_length=100, verbose_name='نشانی')
+
+
+    def __str__(self):
+        return self.title
+    
+
+    class Meta:
+        verbose_name = 'تماس'
+        verbose_name_plural = 'تماس ها'
+
+
+class Footer(models.Model):
+    title = models.CharField(max_length=100, verbose_name='عنوان')
+    address =models.CharField(max_length=100, verbose_name='آدرس')
+    phone = models.CharField(max_length=100, verbose_name='شماره های تماس')
+    email = models.EmailField(verbose_name='ایمیل')
+    whatsapp = models.CharField(max_length=100, verbose_name='واتساپ')
+    facebook = models.CharField(max_length=100, verbose_name='فسبوک')
+
+
+    def __str__(self):
+        return self.title
+    
+
+    class Meta:
+        verbose_name = 'شبکه های اجتماعی'
+        verbose_name_plural = 'شبکه های اجتماعی'
--- home/views.py
+++ home/views.py
@@ -6,8 +6,14 @@ def home(request):
     helpers = Helpers.objects.all()
     details_donation = Details_Donations.objects.all().last()
     pictures = Pictures.objects.all()
+    title_expenses = Title_Expenses.objects.all().last()
+    expenses = Expenses.objects.all()
+    contact = Contact.objects.all().last()
+    footer = Footer.objects.all().last()
 
     return render(request, "home/index.html", 
                   context={'about': about, 'donation_title': donation_title,
                            'helpers': helpers, 'details_donations': details_donation,
-                           'pictures': pictures})
+                           'pictures': pictures, 'title_expenses': title_expenses,
+                           'expenses': expenses, 'contact': contact,
+                           'footer': footer})
